33e9f26c2ee0c11aa82a592ea3f324efd20b5442
LenoxAPI <136239372+LenoxAPI@users.noreply.github.com>
2025-03-11 09:41:09 +0100

    Create worldtoscreen.cs

---
+++ worldtoscreen.cs
@@ -0,0 +1,26 @@
+public static Vector2 WorldToScreen2(ViewMatrix matrix, Vector3 pos, int width, int height)
+{
+    Vector2 screenCoordinates = new Vector2();
+
+    float screenW = (matrix.m41 * pos.X) + (matrix.m42 * pos.Y) + (matrix.m43 * pos.Z) + matrix.m44;
+
+    if (screenW > 0.001f)
+    {
+        float screenX = (matrix.m11 * pos.X) + (matrix.m12 * pos.Y) + (matrix.m13 * pos.Z) + matrix.m14;
+        float screenY = (matrix.m21 * pos.X) + (matrix.m22 * pos.Y) + (matrix.m23 * pos.Z) + matrix.m24;
+
+        float camX = width / 2;
+        float camY = height / 2;
+
+        float X = camX + (camX * screenX / screenW);
+        float Y = camY - (camY * screenY / screenW);
+
+        screenCoordinates.X = X;
+        screenCoordinates.Y = Y;
+        return screenCoordinates;
+    }
+    else
+    {
+        return new Vector2(-99, -99);
+    }
+}
