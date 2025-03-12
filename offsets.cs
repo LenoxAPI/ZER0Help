45f45b2cac785727ec30abef7e43d4ac65a1ee00
LenoxAPI <136239372+LenoxAPI@users.noreply.github.com>
2025-03-12 09:51:14 +0100

    Create offsets.cs

---
+++ offsets.cs
@@ -0,0 +1,11 @@
+int dwEntityList = 0x1A37A30;
+int dwViewMatrix = 0x1AA3810;
+int dwViewAngles = 0x1AADAA0;
+int dwLocalPlayerPawn = 0x188BF30;
+
+int m_vOldOrigin = 0x1324;
+int m_iTeamNum = 0x3E3;
+int m_lifeState = 0x348;
+int m_hPlayerPawn = 0x80C;
+int m_vecViewOffset = 0xCB0;
+int m_iHealth = 0x344;
